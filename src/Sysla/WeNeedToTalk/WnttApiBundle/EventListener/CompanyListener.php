e8ad713f3c3e3671ae57e852860aded5d2a43a94
ulff <ulff.pl@gmail.com>
2015-09-15 14:45:24 +0200

    WNTT-83 Update presentation companyName on company editing

--- src/Sysla/WeNeedToTalk/WnttApiBundle/EventListener/CompanyListener.php
+++ src/Sysla/WeNeedToTalk/WnttApiBundle/EventListener/CompanyListener.php
@@ -5,6 +5,7 @@ namespace Sysla\WeNeedToTalk\WnttApiBundle\EventListener;
 use Doctrine\ODM\MongoDB\Event\LifecycleEventArgs;
 use Sysla\WeNeedToTalk\WnttApiBundle\Document\Company;
 use Sysla\WeNeedToTalk\WnttUserBundle\Document\User;
+use Sysla\WeNeedToTalk\WnttApiBundle\Document\Presentation;
 
 class CompanyListener
 {
@@ -25,6 +26,28 @@ class CompanyListener
             foreach($users as $user) {
                 $user->setCompany(null);
                 $dm->persist($user);
+            }
+            $dm->flush();
+        }
+    }
+
+    /**
+     * @param LifecycleEventArgs $eventArgs
+     */
+    public function postUpdate(LifecycleEventArgs $eventArgs)
+    {
+        $dm = $eventArgs->getDocumentManager();
+        $object = $eventArgs->getDocument();
+
+        if ($object instanceof Company) {
+            /** @var $presentations Presentation[] */
+            $presentations = $dm
+                ->getRepository('SyslaWeNeedToTalkWnttApiBundle:Presentation')
+                ->findBy(['company.id' => $object->getId()]);
+
+            foreach($presentations as $presentation) {
+                $presentation->setCompanyName($object->getName());
+                $dm->persist($presentation);
             }
             $dm->flush();
         }
