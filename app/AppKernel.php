59d52458aec6486dafd62da52bb8175338569df1
ulff <ulff.pl@gmail.com>
2015-09-30 11:47:44 +0200

    inverstigating CircleCI fails

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -53,13 +53,4 @@ class AppKernel extends Kernel
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
 
-    public function getCacheDir()
-    {
-        return '/tmp/sf2/cache/' . $this->environment;
-    }
-
-    public function getLogDir()
-    {
-        return '/tmp/sf2/logs';
-    }
 }
